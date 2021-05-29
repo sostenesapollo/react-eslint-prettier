cc3c5f638ebc081f167737b4c15f16eb09d28f3e
sostenesapollo <sostenesapollo25@gmail.com>
2021-05-29 13:25:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+
+const homes = [
+  { name: 'mi casa 1' },
+  { name: 'mi casa 2' },
+  { name: 'mi casa 3' },
+  { name: 'mi casa 4' },
+  { name: 'mi casa 5' },
+];
+
+function getText() {
+  return 'da fuck';
+}
 
 function App() {
   return (
     <div className="App">
+      <p>{getText()}</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +30,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React{' '}
+          {homes.map((home) => (
+            <p>{home.name}</p>
+          ))}
         </a>
       </header>
     </div>
